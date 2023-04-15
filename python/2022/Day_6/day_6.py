8534b1e5f74c8349be6a9ea363bae67e3221b4bc
AlexLapy <alexandre.lapointe.4@etsmtl.net>
2023-04-15 18:56:42 -0400

    End of day 6

--- python/2022/Day_6/day_6.py
+++ python/2022/Day_6/day_6.py
@@ -9,12 +9,34 @@ class Day_06():
     def __init__(self):
         self.PuzzleInput = my_helper.ReadAllLine("Day_6/day_6.txt")
 
-    # 
+    #
     def PrintSolutionA(self):
-        print("Not implemented yet")
+        print(self.find_packet_marker())
 
-    # 
+    #
     def PrintSolutionB(self):
-        print("Not implemented yet")
+        print(self.find_messsage_marker())
 
-    
+    def find_packet_marker(self):
+
+        signal = self.PuzzleInput[0]
+        string_len = 4
+        for index in range(string_len, len(signal)):
+
+            current_packet = signal[index-string_len:index]
+            
+            is_a_start_of_packet = len(set(current_packet)) == string_len
+            if is_a_start_of_packet:
+                return index
+            
+    def find_messsage_marker(self):
+
+        signal = self.PuzzleInput[0]
+        string_len = 14
+        for index in range(string_len, len(signal)):
+
+            current_packet = signal[index-string_len:index]
+            
+            is_a_start_of_packet = len(set(current_packet)) == string_len
+            if is_a_start_of_packet:
+                return index
